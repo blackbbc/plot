0811ad34fde18de93b8b89999a0ca4b35859b12c
Sweet <ly505968815@gmail.com>
2015-10-30 15:03:58 +0800

    优化缩放

--- bilibili/hello.h
+++ bilibili/hello.h
@@ -19,9 +19,8 @@ INT Y_TICK_PIXEL = 60;
 INT X_TICK_LABEL = 2;
 INT Y_TICK_LABEL = 2;
 
-std::vector<INT> PIXEL_TYPE = { 60, 59, 58, 57, 56, 55, 54, 53, 52, 51,
-								50, 49, 48, 47, 46, 45, 44, 43, 42, 41,
-								40, 39, 38, 37, 36, 35, 34, 33, 32, 31 };
+std::vector<INT> PIXEL_TYPE = { 60, 58, 56, 54, 52, 
+								50, 48, 46, 44, 42 };
 
 std::vector<INT> DISTANCE_TYPE = { 1, 2, 5 };
 
