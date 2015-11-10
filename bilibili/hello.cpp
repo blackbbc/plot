6de98ed1d0972cd7b45508a6fa3ee18bf66d32cb
Sweet <ly505968815@gmail.com>
2015-11-10 15:52:32 +0800

    list veiw 颜色

--- bilibili/hello.cpp
+++ bilibili/hello.cpp
@@ -51,6 +51,87 @@ HWND functionDialog = NULL;
 void updateUI(HWND);
 void updateResolution();
 
+LRESULT ProcessCustomDraw(LPARAM lParam)
+{
+	LPNMLVCUSTOMDRAW lplvcd = (LPNMLVCUSTOMDRAW)lParam;
+
+	switch (lplvcd->nmcd.dwDrawStage)
+	{
+	case CDDS_PREPAINT: //Before the paint cycle begins
+	{
+		//request notifications for individual listview items
+		return CDRF_NOTIFYITEMDRAW;
+	}
+
+	case CDDS_ITEMPREPAINT: //Before an item is drawn
+	{
+		return CDRF_NOTIFYSUBITEMDRAW;
+	}
+	break;
+
+	case CDDS_SUBITEM | CDDS_ITEMPREPAINT: //Before a subitem is drawn
+	{
+		if (lplvcd->nmcd.dwItemSpec == 0)
+		{
+			DebugOut() << "HHHH";
+		}
+		switch (lplvcd->iSubItem)
+		{
+		case 0:
+		{
+			lplvcd->clrText = RGB(255, 255, 255);
+			lplvcd->clrTextBk = RGB(240, 55, 23);
+			return CDRF_NEWFONT;
+		}
+		break;
+
+		case 1:
+		{
+			lplvcd->clrText = RGB(255, 255, 0);
+			lplvcd->clrTextBk = RGB(0, 0, 0);
+			return CDRF_NEWFONT;
+		}
+		break;
+
+		case 2:
+		{
+			lplvcd->clrText = RGB(20, 26, 158);
+			lplvcd->clrTextBk = RGB(200, 200, 10);
+			return CDRF_NEWFONT;
+		}
+		break;
+
+		case 3:
+		{
+			lplvcd->clrText = RGB(12, 15, 46);
+			lplvcd->clrTextBk = RGB(200, 200, 200);
+			return CDRF_NEWFONT;
+		}
+		break;
+
+		case 4:
+		{
+			lplvcd->clrText = RGB(120, 0, 128);
+			lplvcd->clrTextBk = RGB(20, 200, 200);
+			return CDRF_NEWFONT;
+		}
+		break;
+
+		case 5:
+		{
+			lplvcd->clrText = RGB(255, 255, 255);
+			lplvcd->clrTextBk = RGB(0, 0, 150);
+			return CDRF_NEWFONT;
+		}
+		break;
+
+		}
+
+	}
+	}
+	return CDRF_DODEFAULT;
+}
+
 void countRange()
 {
 	//X£¬YµÄ·¶Î§¹Ì¶¨
@@ -879,6 +960,7 @@ HDC settingDC = NULL;
 HDC settingMemDC = NULL;
 HBITMAP settingMemBM = NULL;
 PAINTSTRUCT settingPs;
+
 INT_PTR CALLBACK Setting(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
@@ -1168,10 +1250,24 @@ INT_PTR CALLBACK Setting(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 			}
 			return (INT_PTR)TRUE;
 		}
-		default:
-			break;
-		}
 		break;
+		}
+	}
+	case WM_NOTIFY:
+	{
+		switch (LOWORD(wParam))
+		{
+			case IDC_FUNCTION_LIST:
+			{
+				LPNMLISTVIEW pnm = (LPNMLISTVIEW)lParam;
+				HWND listView = GetDlgItem(hDlg, IDC_FUNCTION_LIST);
+				if (pnm->hdr.hwndFrom == listView && pnm->hdr.code == NM_CUSTOMDRAW)
+				{
+					SetWindowLong(hDlg, DWLP_MSGRESULT, (LONG)ProcessCustomDraw(lParam));
+					return TRUE;
+				}
+			}
+		}
 	}
 	case WM_PAINT:
 		settingDC = BeginPaint(hDlg, &settingPs);
