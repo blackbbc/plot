a29bb32ad88d6c8081f9a665e0dde629b4081ef9
Sweet <ly505968815@gmail.com>
2015-11-20 09:16:59 +0800

    准备性能调优

--- bilibili/hello.cpp
+++ bilibili/hello.cpp
@@ -517,7 +517,15 @@ void onPaint()
 {
 	drawGrid();
 	drawCoordinate();
+
+	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+
 	drawTick();
+
+	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+	DebugOut() << duration;
+
 	drawFunction();
 }
 
@@ -1468,7 +1476,6 @@ INT_PTR CALLBACK Setting(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 					GetDlgItemText(hDlg, IDC_X_RANGE_LEFT, buffer, 128);
 					X_RANGE_LEFT = _wtof(buffer);
 					countProperConfig();
-					//countTickSpace();
 					invalidWindow(functionDialog);
 					delete buffer;
 				}
